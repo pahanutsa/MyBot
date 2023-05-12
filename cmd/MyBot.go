1d38f8a8d300552e1b1096bc5232013fc7e5c1b9
pahanutsa <72981189+pahanutsa@users.noreply.github.com>
2023-05-12 15:30:10 +0000

    Set up Bot basis

--- cmd/MyBot.go
+++ cmd/MyBot.go
@@ -1,18 +1,28 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	//TeleToken
+
+	TeleToken = os.Getenv("TELE_TOKEN")
 )
 
 // MyBotCmd represents the MyBot command
 var MyBotCmd = &cobra.Command{
 	Use:   "MyBot",
+	Aliases: []string{"start"},
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,7 +31,25 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("MyBot called")
+
+		fmt.Printf("MyBot %s started", appVersion)
+		MyBot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			return
+		}
+
+		MyBot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+			return err
+
+		})
+		MyBot.Start()
 	},
 }
 
